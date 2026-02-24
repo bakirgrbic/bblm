8c747af8879e07343392487dac23137296f29070
Bakir Grbic <97543350+bakirgrbic@users.noreply.github.com>
2026-02-23 23:17:05 -0700

    adds tests for my model and a rough draft of config

--- tests/test_my_auto_text_classifier.py
+++ tests/test_my_auto_text_classifier.py
@@ -1,7 +1,7 @@
 import pytest
 from transformers import AutoTokenizer
 
-from bblm.my_auto_text_classifer import MyAutoModelTextClassifier
+from bblm.my_auto_text_classifier.modeling import MyAutoModelTextClassifier
 
 
 @pytest.fixture
@@ -32,3 +32,47 @@ def test_forward_pass_raise_no_error(model_name, sample_data):
         return_tensors="pt",
     )
     model(inputs["input_ids"], inputs["attention_mask"])
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        pytest.param(
+            "bakirgrbic/electra-tiny",
+            id="electra",
+        ),
+        pytest.param(
+            "google-bert/bert-base-uncased",
+            id="bert",
+        ),
+    ],
+)
+def test_save_no_error(model_name, tmp_path):
+    model = MyAutoModelTextClassifier(model_name, num_out=2)
+
+    model.save_pretrained(tmp_path)
+
+    assert (tmp_path / "config.json").exists()
+    assert (tmp_path / "model.safetensors").exists()
+
+
+@pytest.mark.parametrize(
+    "model_name",
+    [
+        pytest.param(
+            "bakirgrbic/electra-tiny",
+            id="electra",
+        ),
+        pytest.param(
+            "google-bert/bert-base-uncased",
+            id="bert",
+        ),
+    ],
+)
+def test_load_no_error(model_name, tmp_path):
+    model = MyAutoModelTextClassifier(model_name, num_out=2)
+
+    model.save_pretrained(tmp_path)
+
+    MyAutoModelTextClassifier.from_config(tmp_path)
+    MyAutoModelTextClassifier.from_pretrained(tmp_path)
